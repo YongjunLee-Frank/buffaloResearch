cef934fd6b459a8a64a654650857d0f2be0fac38
yongjunlee <frank.virus0325@gmail.com>
2023-04-08 23:11:01 -0400

    create server.py

---
+++ server.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask import request
+from flask import Response
+from flask import stream_with_context
+
+from camera_side import Streamer
+
+app = Flask( __name__ )
+streamer = Streamer()
+
+@app.route('/stream')
+def stream():
+    
+    src = request.args.get( 'src', default = 0, type = int )
+    
+    try :
+        print('before send response')
+        return Response(
+                    stream_with_context( stream_gen( src ) ),
+                    mimetype='multipart/x-mixed-replace; boundary=frame'
+                    )
+        
+        print('after send response')
+    except Exception as e :
+        
+        print('[wandlab] ', 'stream error : ',str(e))
+
+def stream_gen( src ):
+  
+    try :
+        
+        streamer.run( src )
+        
+        while True :
+            
+            frame = streamer.bytescode()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            #print(frame)
+    except GeneratorExit :
+        #print( '[wandlab]', 'disconnected stream' )
+        streamer.stop()
