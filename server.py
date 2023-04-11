8ed195114d95b26c7aad623da8da6b36c11c1d0b
yongjunlee <frank.virus0325@gmail.com>
2023-04-10 21:55:24 -0400

    update camera and server code

--- server.py
+++ server.py
@@ -1,42 +1,46 @@
-from flask import Flask
-from flask import request
-from flask import Response
-from flask import stream_with_context
+# Import the required modules
 
-from camera import Streamer
+import socket
+import cv2
+import pickle
+#import numpy as np
+import struct ## new
 
-app = Flask( __name__ )
-streamer = Streamer()
+HOST=''
+PORT=8485
 
-@app.route('/stream')
-def stream():
-    
-    src = request.args.get( 'src', default = 0, type = int )
-    
-    try :
-        print('before send response')
-        return Response(
-                    stream_with_context( stream_gen( src ) ),
-                    mimetype='multipart/x-mixed-replace; boundary=frame'
-                    )
-        
-        print('after send response')
-    except Exception as e :
-        
-        print('[wandlab] ', 'stream error : ',str(e))
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+print('Socket created')
 
-def stream_gen( src ):
-  
-    try :
-        
-        streamer.run( src )
-        
-        while True :
-            
-            frame = streamer.bytescode()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-            #print(frame)
-    except GeneratorExit :
-        #print( '[wandlab]', 'disconnected stream' )
-        streamer.stop()
+s.bind((HOST,PORT))
+print('Socket bind complete')
+s.listen(0)
+print('Socket now listening')
+
+conn,addr=s.accept()
+
+data = b""
+payload_size = struct.calcsize(">L")
+print("payload_size: {}".format(payload_size))
+while True:
+    while len(data) < payload_size:
+        data += conn.recv(4096)
+        if not data:
+            cv2.destroyAllWindows()
+            conn,addr=s.accept()
+            continue
+    # receive image row data form client socket
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack(">L", packed_msg_size)[0]
+    #print (payload_size, packed_msg_size, msg_size)
+    while len(data) < msg_size:
+        data += conn.recv(4096)
+    frame_data = data[:msg_size]
+    data = data[msg_size:]
+    # unpack image using pickle
+    frame=pickle.loads(frame_data, fix_imports=True, encoding="bytes")
+    frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+
+    cv2.imshow('server',frame)
+    cv2.waitKey(1)
