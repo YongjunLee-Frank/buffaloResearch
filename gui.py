cd32c6a44310dbee90352c7aa545d986896456c4
yongjunlee <frank.virus0325@gmail.com>
2023-04-11 00:26:00 -0400

    feat: show streaming in gui frame

--- gui.py
+++ gui.py
@@ -23,8 +23,34 @@ print('Socket now listening')
 
 conn,addr=s.accept()
 
+data = b""
+payload_size = struct.calcsize(">L")
 # this button start to listen camera data from PI side
 def get_video():
+    while True:
+    while len(data) < payload_size:
+        data += conn.recv(4096)
+        if not data:
+            cv2.destroyAllWindows()
+            conn,addr=s.accept()
+            continue
+    # receive image row data form client socket
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack(">L", packed_msg_size)[0]
+    #print (payload_size, packed_msg_size, msg_size)
+    while len(data) < msg_size:
+        data += conn.recv(4096)
+    frame_data = data[:msg_size]
+    data = data[msg_size:]
+    # unpack image using pickle
+    frame=pickle.loads(frame_data, fix_imports=True, encoding="bytes")
+    frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+
+    #cv2.imshow('server',frame)
+    video = Picture(video_box, image = frame, width = 250, height = 250)
+    cv2.waitKey(1)
+
 
 #DECLARE GLOBAL VARIABLES
 count = 0
